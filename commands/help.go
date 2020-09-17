858f36c46e2b5c5c2a6c4aa16fba118afa1664c3
art <art@protonmail.com>
2020-09-18 01:41:40 +0300

    Support slash commands

---
+++ commands/help.go
@@ -0,0 +1,32 @@
+package commands
+
+import "fmt"
+
+var Help = map[string]string{
+	"help [COMMAND]": "help - список команд",
+	"template":       "template { list | add | edit | delete } - работа с шаблонами",
+}
+
+var FullHelp = map[string]string{
+	"template": `
+		list - список шаблонов
+		add CHANNEL [MSGID] - добавить шаблон для канала CHANNEL и сообщения MSGID (или нового сообщения)
+		edit TEMPLATE_ID - ввести новую версию шаблона
+		delete TEMPLATE_ID - удалить шаблон
+	`,
+}
+
+func GetHelp(cmd string) string {
+	var help string
+	if cmd == "" {
+		for _, h := range Help {
+			help += fmt.Sprintln(h)
+		}
+	} else if cmdHelp, ok := Help[cmd]; ok {
+		help = cmdHelp
+		if fullHelp, ok := FullHelp[cmd]; ok {
+			help += fmt.Sprintln(fullHelp)
+		}
+	}
+	return fmt.Sprintln("```") + help + fmt.Sprintln("```")
+}
