db3ad73b7babeb90700a1f666b842bcb6b479719
art <art@protonmail.com>
2020-09-21 20:56:11 +0300

    Entities HTML encoder

--- richtext/richtext.go
+++ richtext/richtext.go
@@ -2,6 +2,7 @@ package richtext
 
 import (
 	"19u4n4/roebot/richtext/style"
+	"fmt"
 
 	t "github.com/go-telegram-bot-api/telegram-bot-api"
 )
@@ -9,6 +10,27 @@ import (
 type Entity struct {
 	Style style.Style
 	Text  string
+}
+
+func (e Entity) String() string {
+	switch e.Style {
+	case style.Plain:
+		return e.Text
+	case style.Bold:
+		return fmt.Sprintf("<b>%s</b>", e.Text)
+	case style.Italic:
+		return fmt.Sprintf("<i>%s</t>", e.Text)
+	case style.Underline:
+		return fmt.Sprintf("<u>%s</u>", e.Text)
+	case style.Strikethrough:
+		return fmt.Sprintf("<s>%s</s>", e.Text)
+	case style.Code:
+		return fmt.Sprintf("<code>%s</code>", e.Text)
+	case style.Pre:
+		return fmt.Sprintf("<pre>%s</pre>", e.Text)
+	default:
+		return e.Text
+	}
 }
 
 func MessageToEntities(msg *t.Message) []Entity {
@@ -36,6 +58,10 @@ func MessageToEntities(msg *t.Message) []Entity {
 	return ret
 }
 
-func EntitiesToHTML([]Entity) string {
-	return ""
+func EntitiesToHTML(es []Entity) string {
+	ret := ""
+	for _, ent := range es {
+		ret += ent.String()
+	}
+	return ret
 }
